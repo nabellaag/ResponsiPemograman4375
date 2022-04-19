78c3f06454d1304bbdec485c82839a128e791068
nabellaag <nabellaayug@students.amikom.ac.id>
2022-04-19 12:00:22 +0700

    responsi

---
+++ ResponsiPemograman4375/Karyawan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman4375
+{
+    public class Karyawan
+    {
+        public string Nama { get; set; }
+        public string NIK { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public void daftar()
+        {
+
+            Console.WriteLine(" Nama : {0}", Nama);
+            Console.WriteLine(" NIK : {0}", NIK);
+            Console.WriteLine(" Gaji Bualanan : {0}", GajiBulanan);
+
+            Console.WriteLine("Semua karyawan telah mendapatkan kenaikan gaji");
+
+        }
+
+        public void gaji()
+        {
+            Console.WriteLine(" Gaji Bualanan : {0}", GajiBulanan);
+
+            
+        }
+
+    }
+}
---
+++ ResponsiPemograman4375/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ResponsiPemograman4375 // project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objek karyawan
+            Karyawan k1 = new Karyawan();
+            Karyawan k2 = new Karyawan();
+
+
+            k1.Nama = "Nabella";
+            k1.NIK = "21114375";
+            k1.GajiBulanan = 5000000;
+
+            k2.Nama = "Nabella";
+            k2.NIK = "21114375";
+            k2.GajiBulanan = 5000000;
+
+
+            k1.daftar();
+            k2.daftar();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
